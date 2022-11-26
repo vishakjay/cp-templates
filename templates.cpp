bc69dc7749e791bbe437079ddfd01c14844ecec8
vishakjay <107393021+vishakjay@users.noreply.github.com>
2022-11-26 19:40:21 +0530

    Update templates.cpp

--- templates.cpp
+++ templates.cpp
@@ -1,10 +1,60 @@
-#include <bits/stdc++.h>
-#include <chrono>
+#include<bits/stdc++.h>
 using namespace std;
-#define wrep(t) while(t--);
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+#define PI 3.1415926535897932384626433832795l 
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+// -------------------------<RNG>------------------------- 
+// RANDOM NUMBER GENERATOR
+mt19937 RNG(chrono::steady_clock::now().time_since_epoch().count());  
+#define SHUF(v) shuffle(all(v), RNG); 
+// Use mt19937_64 for 64 bit random numbers.
+void solve(){
+   
+}
 
 int main(){
+/*Uncomment the multi line comment to run the code in sublime text if you are using an input.txt and the output.txt file
+  and make sure the .cpp file and the input.txt and the output.txt are in the same directory and make sure to use sublime text 3  because there are some bugs
+  in sublime text 4  currently with the auto complete option*/
+/*#ifndef ONLINE_JUDGE
 
+freopen("input.txt","r",stdin);
 
- return 0;
+freopen("output.txt","w",stdout);
+
+#endif*/
+ios_base::sync_with_stdio(0);
+cin.tie(0); cout.tie(0);
+
+int t;
+cin>>t;
+ 
+ while(t--){
+ 	solve();
+ }
+
+return 0;	
 }
+ 
